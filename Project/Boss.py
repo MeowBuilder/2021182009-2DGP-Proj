8d048af05d7cd6ffd49c09509756f14126da0449
태순 김(노트북) <96215662+MeowBuilder@users.noreply.github.com>
2024-10-14 14:16:25 +0900

    Boss 틀 제작

---
+++ Project/Boss.py
@@ -0,0 +1,13 @@
+from pico2d import *
+
+class Boss:
+    def __iniit__(self):
+        self.HP = 10
+        self.image
+        self.x, self.y = 640, 360
+        self.cur_pattern = 0
+        self.next_pattern = 1
+    def update(self):
+        pass
+    def draw(self):
+        pass
