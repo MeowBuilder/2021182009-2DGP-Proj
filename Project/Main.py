63e95c86545c5f6748d66d7cfb80dd4912745326
TaesoonKim@Home <tta122057@gmail.com>
2024-10-15 22:46:31 +0900

    handle_events 플레이어 내부로 이동 및 각 State에 따른 do 정의

--- Project/Main.py
+++ Project/Main.py
@@ -10,48 +10,12 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running = False
-            elif event.key == SDLK_m: # HP 닳기 확인용
-                player.HP -= 1
-            elif event.key == SDLK_s:
-                player.switch_dir(0)
-                player.switch_state(Move)
-            elif event.key == SDLK_w:
-                player.switch_dir(1)
-                player.switch_state(Move)
-            elif event.key == SDLK_d:
-                player.switch_dir(2)
-                player.switch_state(Move)
-            elif event.key == SDLK_a:
-                player.switch_dir(3)
-                player.switch_state(Move)
-            elif event.key == SDLK_LSHIFT:
-                player.switch_state(Dash)
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_s:
-                if player.dir[0] and player.state_machine.cur_state == Move:
-                    player.switch_state(Idle)
-            elif event.key == SDLK_w:
-                if player.dir[1] and player.state_machine.cur_state == Move:
-                    player.switch_state(Idle)
-            elif event.key == SDLK_d:
-                if player.dir[2] and player.state_machine.cur_state == Move:
-                    player.switch_state(Idle)
-            elif event.key == SDLK_a:
-                if player.dir[3] and player.state_machine.cur_state == Move:
-                    player.switch_state(Idle)
-        elif event.type == SDL_MOUSEBUTTONDOWN:
-            if event.button == SDL_BUTTON_LEFT:
-                if player.state_machine.cur_state == Attack_1:
-                    player.attack_side = 1
-                elif player.state_machine.cur_state == Attack_2:
-                    player.attack_side = 2
-                else:
-                    player.frame = 0
-                    player.attack_side = 0
-                    player.switch_state(Attack_1)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_m: # HP 닳기 확인용
+            player.HP -= 1
+        else:
+            player.handle_events(event)
     
 
 def update_world():
