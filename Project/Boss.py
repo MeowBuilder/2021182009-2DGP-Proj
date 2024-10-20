52906d5154dd5256eae01959c34c347f8f80869c
TaesoonKim@Home <tta122057@gmail.com>
2024-10-20 19:26:19 +0900

    Boss Idle, Attack 간단하게 구현

--- Project/Boss.py
+++ Project/Boss.py
@@ -4,12 +4,14 @@ import State_Machine
 class Boss:
     def __init__(self):
         self.idle_sprite = load_image('./Asset/Boss/idle.png')
+        self.attack_sprite = load_image('./Asset/Boss/attacking.png')
         self.frame = 0
         
         self.HP = 10
         self.x, self.y = 640, 360
         self.cur_pattern = 0
         self.next_pattern = 1
+        self.idle_time = 0
         
         self.state_machine = State_Machine.StateMachine(self)
         self.state_machine.start(Idle)
@@ -25,6 +27,7 @@ class Boss:
 class Idle:
     @staticmethod
     def enter(Boss):
+        Boss.idle_time = 0
         pass
     
     @staticmethod
@@ -33,8 +36,33 @@ class Idle:
     
     @staticmethod
     def do(Boss):
-        Boss.frame = (Boss.frame + 1) % 4
+        Boss.idle_time += 0.5
+        Boss.frame = (Boss.frame + 1) % 8
+        
+        if Boss.idle_time >= 30:
+            Boss.state_machine.start(Attack)
     
     @staticmethod
     def draw(Boss):
-        Boss.idle_sprite.clip_composite_draw(Boss.frame*100,0,100,100,0,'h',Boss.x,Boss.y,128,128)
+        Boss.idle_sprite.clip_composite_draw(Boss.frame*100,0,100,100,0,'h',Boss.x,Boss.y,256,256)
+        
+class Attack:
+    @staticmethod
+    def enter(Boss):
+        Boss.frame = 0
+        pass
+    
+    @staticmethod
+    def exit(Boss):
+        Boss.frame = 0
+        pass
+    
+    @staticmethod
+    def do(Boss):
+        Boss.frame = (Boss.frame + 1)
+        if Boss.frame == 13:
+            Boss.state_machine.start(Idle)
+    
+    @staticmethod
+    def draw(Boss):
+        Boss.attack_sprite.clip_composite_draw(Boss.frame*100,0,100,100,0,'h',Boss.x,Boss.y,256,256)
